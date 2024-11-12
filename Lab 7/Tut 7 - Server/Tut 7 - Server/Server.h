e1e60a9d3e8f889e4c1eff11053c11b6ad8c241c
Josh <joshua.boycehyland15@gmail.com>
2024-11-12 09:20:55 +0000

    Added some function summarrys

--- Lab 7/Tut 7 - Server/Tut 7 - Server/Server.h
+++ Lab 7/Tut 7 - Server/Tut 7 - Server/Server.h
@@ -14,24 +14,50 @@ enum Packet
 class Server
 {
 public:
+	/// <summary>
+	/// Initializes winsocka and sets adress info to port number and whether it should broadcast punlicly
+	/// </summary>
+	/// <param name="PORT">Port number</param>
+	/// <param name="BroadcastPublically">Whether to broadcast publially</param>
 	Server(int PORT, bool BroadcastPublically = false);
+
+	/// <summary>
+	/// Listen for connections to accept
+	/// if their is connections it will creat a threat ans send a string of total connections
+	/// </summary>
+	/// <returns>WHther is got a new connection or not</returns>
 	bool ListenForNewConnection();
 
 private:
+
+	//sending / recieving functions  
 	bool sendall(int ID, char * data, int totalbytes);
 	bool recvall(int ID, char * data, int totalbytes);
 
+	// int sending/ recieving
 	bool SendInt(int ID, int _int);
 	bool GetInt(int ID, int & _int);
 
+	//Packet type sending/ recieving
 	bool SendPacketType(int ID, Packet _packettype);
 	bool GetPacketType(int ID, Packet & _packettype);
 
+	//string sending/ recieving
 	bool SendString(int ID, std::string & _string);
 	bool GetString(int ID, std::string & _string);
 
+	/// <summary>
+	/// Processes any new messages 
+	/// </summary>
+	/// <param name="ID"></param>
+	/// <param name="_packettype"></param>
+	/// <returns>Whether we recieved any messages</returns>
 	bool ProcessPacket(int ID, Packet _packettype);
 
+	/// <summary>
+	/// Handles the our connectsion and if we need to close certain sockets
+	/// </summary>
+	/// <param name="ID"></param>
 	static void ClientHandlerThread(int ID);
 
 private:
